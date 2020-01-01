c5e3baf7593df56eeed19eef4174a48322f087e7
vaddya <vadim.dyachkov@corp.mail.ru>
2020-01-02 01:41:19 +0300

    fix parentheses task

--- principles/src/main/scala/com/vaddya/fpscala/principles/week1/ParenthesesBalancing.scala
+++ principles/src/main/scala/com/vaddya/fpscala/principles/week1/ParenthesesBalancing.scala
@@ -9,16 +9,14 @@ import scala.annotation.tailrec
 object ParenthesesBalancing extends App {
   def balance(chars: List[Char]): Boolean = {
     @tailrec
-    def iter(chars: List[Char], stack: List[Char]): Boolean = {
-      chars match {
-        case Nil => true
-        case '(' :: ctail => iter(ctail, '(' :: stack)
-        case ')' :: ctail => stack match {
-          case '(' :: stail => iter(ctail, stail)
-          case _ => false
-        }
-        case _ => iter(chars.tail, stack)
+    def iter(chars: List[Char], stack: List[Char]): Boolean = chars match {
+      case Nil => stack.isEmpty
+      case '(' :: ctail => iter(ctail, '(' :: stack)
+      case ')' :: ctail => stack match {
+        case '(' :: stail => iter(ctail, stail)
+        case _ => false
       }
+      case _ => iter(chars.tail, stack)
     }
 
     iter(chars, Nil)
@@ -28,4 +26,5 @@ object ParenthesesBalancing extends App {
   assert(balance("I told him (that it’s not (yet) done). (But he wasn’t listening)".toList))
   assert(!balance(":-)".toList))
   assert(!balance("())(".toList))
+  assert(!balance("()(".toList))
 }
