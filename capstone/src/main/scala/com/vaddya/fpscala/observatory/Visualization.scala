6a9e0cdcab37a9a229902d4fb39e02588687b5e3
vaddya <vadik.dyachkov@gmail.com>
2020-07-30 14:19:29 +0300

    fix indent

--- capstone/src/main/scala/com/vaddya/fpscala/observatory/Visualization.scala
+++ capstone/src/main/scala/com/vaddya/fpscala/observatory/Visualization.scala
@@ -14,14 +14,14 @@ object Visualization extends VisualizationInterface {
   val ImageHeight = 180
   val Alpha = 255
   val Colors = Seq(
-    (60d,  Color(255, 255,	255)),
-    (32d,  Color(255, 0,	  0)),
-    (12d,  Color(255, 255,	0)),
-    (0d,   Color(0,   255,	255)),
-    (-15d, Color(0,   0,	  255)),
-    (-27d, Color(255, 0,	  255)),
-    (-50d, Color(33,  0,	  107)),
-    (-60d, Color(0,   0,	  0))
+    (60d,  Color(255, 255, 255)),
+    (32d,  Color(255, 0,   0)),
+    (12d,  Color(255, 255, 0)),
+    (0d,   Color(0,   255, 255)),
+    (-15d, Color(0,   0,   255)),
+    (-27d, Color(255, 0,   255)),
+    (-50d, Color(33,  0,   107)),
+    (-60d, Color(0,   0,   0))
   )
 
   /**
