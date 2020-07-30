c4b9f17f884d1dd2d4eedf8de488d49c6b415f29
vaddya <vadik.dyachkov@gmail.com>
2020-07-31 01:31:18 +0300

    impl interaction

--- capstone/src/main/scala/com/vaddya/fpscala/observatory/Interaction.scala
+++ capstone/src/main/scala/com/vaddya/fpscala/observatory/Interaction.scala
@@ -2,33 +2,63 @@ package com.vaddya.fpscala.observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
 
+import scala.annotation.tailrec
+
 /**
   * 3rd milestone: interactive visualization
   */
 object Interaction extends InteractionInterface {
 
+  import Visualization._
+
+  val ImageWidth = 256
+  val ImageHeight = 256
+  val Alpha = 127
+
   /**
     * @param tile Tile coordinates
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
-  def tileLocation(tile: Tile): Location = {
-    ???
-  }
+  def tileLocation(tile: Tile): Location = Location(
+    latitude(tile.y, tile.zoom),
+    longitude(tile.x, tile.zoom)
+  )
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @param tile Tile coordinates
+    * @param colors       Color scale
+    * @param tile         Tile coordinates
     * @return A 256×256 image showing the contents of the given tile
     */
-  def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    ???
+  def tile(
+    temperatures: Iterable[(Location, Temperature)],
+    colors: Iterable[(Temperature, Color)],
+    tile: Tile
+  ): Image = {
+    def pixel(tile: Tile): Pixel = {
+      val location = tileLocation(tile)
+      val temp = predictTemperature(temperatures, location)
+      val color = interpolateColor(colors, temp)
+      Pixel(color.red, color.green, color.blue, Alpha)
+    }
+
+    @tailrec
+    def generateSubtiles(tiles: Seq[Tile], depth: Int): Seq[Tile] =
+      if (depth == 0) tiles
+      else generateSubtiles(tiles.flatMap(getSubtiles), depth - 1)
+
+    val pixels = generateSubtiles(Seq(tile), 8)
+      .sortBy { case Tile(x, y, _) => (y, x) }
+      .map(pixel)
+      .toArray
+    Image(ImageWidth, ImageHeight, pixels)
   }
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
-    * @param yearlyData Sequence of (year, data), where `data` is some data associated with
-    *                   `year`. The type of `data` can be anything.
+    *
+    * @param yearlyData    Sequence of (year, data), where `data` is some data associated with
+    *                      `year`. The type of `data` can be anything.
     * @param generateImage Function that generates an image given a year, a zoom level, the x and
     *                      y coordinates of the tile and the data to build the image from
     */
@@ -36,7 +66,52 @@ object Interaction extends InteractionInterface {
     yearlyData: Iterable[(Year, Data)],
     generateImage: (Year, Tile, Data) => Unit
   ): Unit = {
-    ???
+    for {
+      (year, data) <- yearlyData
+      zoom <- 0 to 3
+      tile <- generateTiles(zoom)
+    } generateImage(year, tile, data)
   }
 
+  /**
+    * Computes the longitude by the given X coordinate and zoom level
+    */
+  private def longitude(x: Double, zoom: Int): Double =
+    x * 360.0 / tilesNum(zoom) - 180.0
+
+  /**
+    * Computes the latitude by the given Y coordinate and zoom level
+    *
+    * @see https://en.wikipedia.org/wiki/Web_Mercator_projection
+    */
+  private def latitude(y: Double, zoom: Int): Double =
+    math.atan(math.sinh(math.Pi * (1 - 2 * y / tilesNum(zoom)))).toDegrees
+
+  /**
+    * Computes the number of tiles by the given zoom level
+    *
+    * @see https://en.wikipedia.org/wiki/Web_Mercator_projection
+    **/
+  private def tilesNum(zoom: Int): Int = math.pow(2, zoom).toInt
+
+  /**
+    * @see https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Subtiles
+    */
+  private def getSubtiles(tile: Tile): Seq[Tile] = Seq(
+    Tile(tile.x * 2, tile.y * 2, tile.zoom + 1),
+    Tile(tile.x * 2 + 1, tile.y * 2, tile.zoom + 1),
+    Tile(tile.x * 2, tile.y * 2 + 1, tile.zoom + 1),
+    Tile(tile.x * 2 + 1, tile.y * 2 + 1, tile.zoom + 1)
+  )
+
+  /**
+    * Generates all tiles for a given zoom level
+    */
+  private def generateTiles(zoom: Int): Seq[Tile] = {
+    val tiles = tilesNum(zoom)
+    for {
+      y <- 0 until tiles
+      x <- 0 until tiles
+    } yield Tile(x, y, zoom)
+  }
 }
--- capstone/src/main/scala/com/vaddya/fpscala/observatory/Main.scala
+++ capstone/src/main/scala/com/vaddya/fpscala/observatory/Main.scala
@@ -6,11 +6,12 @@ object Main extends App {
   val temperatureFile = "/2015.csv"
   val stationsFile = "/stations.csv"
   val records = Extraction.locateTemperatures(2015, stationsFile, temperatureFile)
-  val avg = Extraction.locationYearlyAverageRecords(records)
+  val temperatures = Extraction.locationYearlyAverageRecords(records)
+  
   val start = System.currentTimeMillis()
-  val img = Visualization.visualize(avg, Visualization.Colors)
+  val img = Interaction.tile(temperatures, Visualization.Colors, Tile(60, 30, 2))
   println(s"Took ${System.currentTimeMillis() - start} ms")
-  val file = new File("2015.png")
+  val file = new File("tile.png")
   println(file.getAbsolutePath)
   img.output(file)
 }
